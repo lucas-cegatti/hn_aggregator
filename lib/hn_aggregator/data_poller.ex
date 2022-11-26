1e57b8bf90d7f31f6b0b4f0ff1c36349bb474554
Lucas Cegatti <me@lucascegatti.io>
2022-11-26 10:22:54 -0300

    Add data poller save feature and schema save and get all functions

--- lib/hn_aggregator/data_poller.ex
+++ lib/hn_aggregator/data_poller.ex
@@ -1,9 +1,22 @@
 defmodule HnAggregator.DataPoller do
   @moduledoc """
-  Module responsible to poll data from HN api and store it into the default Schema at :mnesia
+  Module responsible to poll data from HN api and store it into the given Schema.
+
+  Available configurations are:
+
+  - `poll_interval` interval in seconds to poll the data from HN, defaults to 300.
+  - `hn_endpoint` HN endpoint to poll the data from, defaults to `https://hacker-news.firebaseio.com/v0/topstories.json`.
+  - `max_retries` Number of retries to poll the data before marking a failure at the poll endpoint, default value is 5.
+
+  The data poller will have an exponential backoff in case of failures, the rule is simple, the nex poll will happen after current_retry x 60 so,
+  for instance, the retry after the first failure will happen 1 x 60, after 60 seconds.
   """
 
   use GenServer
+
+  require Logger
+
+  alias HnAggregator.Schema
 
   @hn_top_stories_endpoint "https://hacker-news.firebaseio.com/v0/topstories.json"
 
@@ -14,38 +27,65 @@ defmodule HnAggregator.DataPoller do
   def init(args) do
     poll_interval = Keyword.get(args, :poll_interval, 300)
     hn_endpoint = Keyword.get(args, :hn_endpoint, @hn_top_stories_endpoint)
+    max_retries = Keyword.get(args, :max_retries, 5)
 
     schedule_next_poll(5)
 
-    {:ok, %{poll_interval: poll_interval, data: [], hn_endpoint: hn_endpoint}}
+    {:ok,
+     %{
+       poll_interval: poll_interval,
+       data: [],
+       hn_endpoint: hn_endpoint,
+       retries: 0,
+       max_retries: max_retries
+     }}
   end
 
   @doc """
-  Polls the data from hacker news e pass it to the next function to process.
+  Polls the data from hacker news and pass it to the next function to process.
   """
-  def handle_info(:poll, %{poll_interval: poll_interval, hn_endpoint: hn_endpoint} = state) do
-    schedule_next_poll(poll_interval)
+  def handle_info(
+        :poll,
+        %{poll_interval: poll_interval, hn_endpoint: hn_endpoint, retries: retries} = state
+      ) do
+    with {:ok, {{_, 200, _}, _header, body}} <- :httpc.request(hn_endpoint),
+         {:ok, data} <- parse_response(body) do
+      schedule_next_poll(poll_interval)
 
-    {:ok, data} =
-      :httpc.request(hn_endpoint)
-      |> handle_response()
-      |> parse_response()
-      |> IO.inspect(label: :response)
+      {:noreply, %{state | data: data, retries: 0}, {:continue, :process_response}}
+    else
+      {:ok, {{_, status_code, _}, _header, _body}} ->
+        Logger.error("HTTP call failed due to unsupported status code #{status_code}")
 
-    {:noreply, %{state | data: data}, {:continue, :process_response}}
+        {:noreply, %{state | data: state.data, retries: retries + 1},
+         {:continue, :process_http_invalid_status}}
+
+      {:error, %Jason.DecodeError{} = error} ->
+        Logger.error(error)
+
+        {:noreply, %{state | data: state.data, retries: retries + 1},
+         {:continue, :process_http_invalid_status}}
+    end
   end
 
-  def handle_continue(:process_response, state) do
-    IO.inspect(state.data, label: :process_response)
+  @doc """
+  Handle continue are called after a poll is made on the given HN endpoint. The following are each message:
+
+  - `:process_response` a successfull call was made and no errors were found, it will continue by saving the data at the given Schema.
+  - `process_http_invalid_status` an unsupported http status code was returned, it will increase the number of retries and try again based on its exponential backoff rule
+  """
+  def handle_continue(:process_response, %{data: data} = state) do
+    to_save_data = Enum.take(data, 50)
+
+    Schema.save(to_save_data)
 
     {:noreply, state}
   end
 
-  defp handle_response({:ok, {{_, 200, _}, _header, body}}) do
-    {:ok, body}
+  def handle_continue(:process_http_invalid_status, _state) do
   end
 
-  defp parse_response({:ok, body}) do
+  defp parse_response(body) do
     Jason.decode(body)
   end
 
--- lib/hn_aggregator/schema.ex
+++ lib/hn_aggregator/schema.ex
@@ -1,26 +1,72 @@
 defmodule HnAggregator.Schema do
   @moduledoc """
-  Schema that stores the HN data into memory
+  Schema that stores the HN data into memory.
   """
   use GenServer
 
   alias :mnesia, as: Mnesia
+
+  @hn_data_table HnData
+
+  require Logger
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   def init(args) do
-    create(:mnesia)
+    data_source = Keyword.get(args, :data_source, :mnesia)
 
-    {:ok, args}
+    create(data_source)
+
+    {:ok, %{data_source: data_source}}
   end
 
+  @doc """
+  Saves the given data to the data source
+
+  - `data` is an array of ids to be saved
+  """
+  def save(data) do
+    GenServer.cast(__MODULE__, {:save, data})
+  end
+
+  def get_all() do
+    Mnesia.transaction(fn ->
+      Mnesia.match_object({@hn_data_table, :_, :_})
+    end)
+  end
+
+  @doc """
+  Creates the schema based on its given atom parameter.
+
+  :mnesia -> will create a mnesia schema to save the data and use it as data source
+
+  It's possible to support different types of schemas, if necessary, like ets, for example.
+  """
   def create(:mnesia) do
     Mnesia.create_schema([node()])
 
     Mnesia.start()
 
-    Mnesia.create_table(HnData, attributes: [:id, :fetch_uri], type: :ordered_set)
+    Mnesia.create_table(@hn_data_table, attributes: [:id, :fetch_uri], type: :ordered_set)
+  end
+
+  def handle_cast({:save, data}, %{data_source: :mnesia} = state) do
+    Mnesia.transaction(fn ->
+      Mnesia.clear_table(@hn_data_table)
+    end)
+
+    Mnesia.transaction(fn ->
+      Enum.map(data, fn item_id ->
+        Mnesia.write(
+          {@hn_data_table, item_id, "https://hacker-news.firebaseio.com/v0/item/#{item_id}.json?"}
+        )
+      end)
+    end)
+
+    Logger.warn("Records successfully saved to mnesia")
+
+    {:noreply, state}
   end
 end
